6d36e2c4487ed9156297fb8f348bc15ba659374d
malak <malak.lakehal20@ump.ac.ma>
2024-08-01 00:31:25 +0100

    css login page

--- client/src/login.js
+++ client/src/login.js
@@ -1,61 +1,94 @@
-import React, { useState } from 'react';
-import { Link,useNavigate } from 'react-router-dom'
-import validation from './loginvalidation';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import validation from "./loginvalidation";
 import axios from "axios";
+import "./login.css";
 
-function Login () {
-    const [values,setValues]= useState
-    ({
-        email: '',
-        password:''
-    })
-    const navigate = useNavigate();
-    const [errors,setErrors]= useState({});
-    const handleInput = (event) => {
-        setValues(prev =>({...prev,[event.target.name]:[event.target.value]}))
-    }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setErrors(validation(values));
-        if (
-            errors.password === "" &&
-            errors.email === ""
-          ) {
-            axios.post("http://localhost:3001/login", values)
-              .then(res => {
-                if (res.data === "success"){
-                   navigate("/"); 
-                }else{alert("no record")}
-                console.log("success");
-                
-              })
-              .catch(err => console.log(err));
+function Login() {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+    if (errors.password === "" && errors.email === "") {
+      axios
+        .post("http://localhost:3001/login", values)
+        .then((res) => {
+          if (res.data === "success") {
+            navigate("/");
+          } else {
+            alert("no record");
           }
+          console.log("success");
+        })
+        .catch((err) => console.log(err));
     }
-    return (
-    <div className='d-flex justify-content-center'>
-            <div className='bg-white ' style={{ borderRadius: '20px', boxShadow: '0px 16px 40px 0px rgba(154, 170, 207, 0.2)', backgroundColor: '#fff', alignSelf: 'center', display: 'flex', marginTop: '86px', width: '590px', maxWidth: '100%', flexDirection: 'column', alignItems: 'center', color: '#4b4b4b', fontWeight: 400, padding: '80px' }}>
-                <form action="" onSubmit={handleSubmit}>
-                    <div className='mb-3'>
-                        <label htmlFor="email">Email</label><br></br>
-                        <input type="email" placeholder='Email' name="email" id="email" 
-                        onChange={handleInput}/>
-                        {errors.email && <span className='text-danger'>{errors.email}</span>}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="password">Mot de passe</label><br></br>
-                        <input type="password" placeholder='Mot de passe' name="password" id="password"
-                        onChange={handleInput}/>
-                        {errors.password && <span className='text-danger'>{errors.password}</span>}
-                    </div>
-                    <p style={{fontSize: '12px'}}>Mot de passe oublié ?</p>
-                    <button type ='submit' className='btn_btn_success'>Se connecter</button> 
-                    <Link to='/addUser' className='btn_btn_success'>ajouter</Link>
-                    
-                </form>
-            </div>
+  };
+  return (
+    <div className="back">
+      <div className="header">
+        <div className="logo">
+          <img srcSet="src/logo.png"/>
+          <div className="div-4">ParcAuto</div>
+        </div>
+        <div className="di">
+       <div>Accueil</div>
+       <div>Services</div>
+       <div>A propos</div>
+       <div>Contact</div>        
     </div>
-    );
-};
+      </div>
+      <div className="box ">
+        <form action="" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">Email</label>
+            <br></br>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              id="email"
+              onChange={handleInput}
+            />
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Mot de passe</label>
+            <br></br>
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
+              id="password"
+              onChange={handleInput}
+            />
+            {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )}
+          </div>
+          <p style={{ fontSize: "12px" }}>Mot de passe oublié ?</p>
+          <button type="submit" className="btn btn-success">
+            Se connecter
+          </button>
+          <Link to="/addUser" className="btn btn-success">
+            ajouter
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
---
+++ client/src/mycss.js
@@ -0,0 +1,373 @@
+import * as React from "react";
+
+function MyComponent() {
+return (
+  <>
+    <div className="div">
+      <div className="div-2">
+        <div className="div-3">
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/66c3b7a8c366406866cfdfa31858d68595cd0f6711b4eecd3a16ae91f00874c4?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+            className="img"
+          />
+          <div className="div-4">ParcAuto</div>
+        </div>
+        <div className="div-5">
+          <div className="div-6">Accueil</div>
+          <div className="div-7">Services</div>
+          <div className="div-8">A propos</div>
+          <div className="div-9">Contact</div>
+        </div>
+      </div>
+      <div className="div-10">
+        <div className="div-11">
+          <div className="div-12">
+            <div className="div-13">S’inscrire</div>
+            <div className="div-14">Email</div>
+          </div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/4fbf5d74e10fc326e4ca331c1d9e5791b2dd6825fc680f96cf3f0936ae9c13b5?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+            className="img-2"
+          />
+        </div>
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/82ad68079a5fe7243c39723353d29ff433f9998906ce4591880e3c743982b33b?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+          className="img-3"
+        />
+        <div className="div-15">
+          <div className="div-16">Nom d’utilisateur</div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/13bcdedea44e22c60ad40e370b5e87b38f70b18d54c7943d6b2f4f8bb85d9b8e?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+            className="img-4"
+          />
+        </div>
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/82ad68079a5fe7243c39723353d29ff433f9998906ce4591880e3c743982b33b?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+          className="img-5"
+        />
+        <div className="div-17">
+          <div className="div-18">Mot de passe</div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/a6ac42af8bf69c3636be6218ffe48f5fe351c8480bab0ddc950db995895995ef?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+            className="img-6"
+          />
+        </div>
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/3b6b359d90d8a5313fcfdbce7a7d3faa6930ec5b0024065fc85d56933691385b?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+          className="img-7"
+        />
+        <div className="div-19">
+          <div className="div-20">Confirmer le mot de passe</div>
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/340de12d49163a1fa5aa4d00e2749b3dd21b262c9ce5574e7e677537edb8cf0c?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+            className="img-8"
+          />
+        </div>
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/82ad68079a5fe7243c39723353d29ff433f9998906ce4591880e3c743982b33b?apiKey=4950322b4f9944b9aa1bf2157e4fb18e&&apiKey=4950322b4f9944b9aa1bf2157e4fb18e"
+          className="img-9"
+        />
+        <div className="div-21">S’inscrire</div>
+      </div>
+    </div>
+    <style jsx>{`
+      .div {
+        background-color: rgba(255, 255, 255, 1);
+        display: flex;
+        padding-bottom: 78px;
+        flex-direction: column;
+      }
+      .div-2 {
+        border-radius: 16px;
+        background-color: rgba(105, 169, 209, 1);
+        display: flex;
+        width: 100%;
+        gap: 20px;
+        color: rgba(255, 255, 255, 1);
+        justify-content: space-between;
+        padding: 6px 70px 26px 16px;
+      }
+      @media (max-width: 991px) {
+        .div-2 {
+          max-width: 100%;
+          flex-wrap: wrap;
+          padding-right: 20px;
+        }
+      }
+      .div-3 {
+        display: flex;
+        gap: 15px;
+        font-size: 36px;
+        font-weight: 800;
+        white-space: nowrap;
+        text-align: center;
+        letter-spacing: 0.72px;
+      }
+      @media (max-width: 991px) {
+        .div-3 {
+          white-space: initial;
+        }
+      }
+      .img {
+        aspect-ratio: 0.85;
+        object-fit: auto;
+        object-position: center;
+        width: 71px;
+      }
+      .div-4 {
+        font-family: Poppins, sans-serif;
+        flex-grow: 1;
+        flex-basis: auto;
+        margin: auto 0;
+      }
+      .div-5 {
+        display: flex;
+        align-items: start;
+        gap: 20px;
+        font-size: 24px;
+        font-weight: 400;
+        justify-content: space-between;
+        margin: auto 0;
+      }
+      @media (max-width: 991px) {
+        .div-5 {
+          max-width: 100%;
+          flex-wrap: wrap;
+        }
+      }
+      .div-6 {
+        font-family: Poppins, sans-serif;
+        white-space: nowrap;
+      }
+      @media (max-width: 991px) {
+        .div-6 {
+          white-space: initial;
+        }
+      }
+      .div-7 {
+        font-family: Poppins, sans-serif;
+        white-space: nowrap;
+      }
+      @media (max-width: 991px) {
+        .div-7 {
+          white-space: initial;
+        }
+      }
+      .div-8 {
+        font-family: Poppins, sans-serif;
+      }
+      .div-9 {
+        font-family: Poppins, sans-serif;
+        align-self: stretch;
+      }
+      .div-10 {
+        border-radius: 20px;
+        background-color: rgba(255, 255, 255, 1);
+        box-shadow: 0px 16px 40px rgba(154, 170, 207, 0.2);
+        align-self: center;
+        display: flex;
+        margin-top: 29px;
+        width: 590px;
+        max-width: 100%;
+        flex-direction: column;
+        align-items: center;
+        color: rgba(75, 75, 75, 1);
+        font-weight: 400;
+        padding: 80px;
+      }
+      @media (max-width: 991px) {
+        .div-10 {
+          padding: 0 20px;
+        }
+      }
+      .div-11 {
+        display: flex;
+        margin-top: 9px;
+        width: 379px;
+        max-width: 100%;
+        align-items: flex-start;
+        gap: 20px;
+        white-space: nowrap;
+        justify-content: space-between;
+      }
+      @media (max-width: 991px) {
+        .div-11 {
+          white-space: initial;
+        }
+      }
+      .div-12 {
+        align-self: start;
+        display: flex;
+        flex-direction: column;
+      }
+      @media (max-width: 991px) {
+        .div-12 {
+          white-space: initial;
+        }
+      }
+      .div-13 {
+        align-self: end;
+        font: 36px Lato, sans-serif;
+      }
+      .div-14 {
+        margin-top: 89px;
+        font: 24px Lato, sans-serif;
+      }
+      @media (max-width: 991px) {
+        .div-14 {
+          margin-top: 40px;
+        }
+      }
+      .img-2 {
+        aspect-ratio: 1;
+        object-fit: auto;
+        object-position: center;
+        width: 22px;
+        align-self: end;
+        margin-top: 118px;
+      }
+      @media (max-width: 991px) {
+        .img-2 {
+          margin-top: 40px;
+        }
+      }
+      .img-3 {
+        object-fit: auto;
+        object-position: center;
+        width: 396px;
+        margin-top: 17px;
+        max-width: 100%;
+        border: 1px solid rgba(75, 75, 75, 1);
+      }
+      .div-15 {
+        display: flex;
+        margin-top: 69px;
+        width: 381px;
+        max-width: 100%;
+        gap: 20px;
+        font-size: 24px;
+      }
+      @media (max-width: 991px) {
+        .div-15 {
+          margin-top: 40px;
+        }
+      }
+      .div-16 {
+        font-family: Lato, sans-serif;
+        flex-grow: 1;
+        flex-basis: auto;
+      }
+      .img-4 {
+        aspect-ratio: 1;
+        object-fit: auto;
+        object-position: center;
+        width: 22px;
+        align-self: start;
+      }
+      .img-5 {
+        object-fit: auto;
+        object-position: center;
+        width: 396px;
+        margin-top: 17px;
+        max-width: 100%;
+        border: 1px solid rgba(75, 75, 75, 1);
+      }
+      .div-17 {
+        display: flex;
+        margin-top: 69px;
+        width: 377px;
+        max-width: 100%;
+        gap: 20px;
+        font-size: 24px;
+      }
+      @media (max-width: 991px) {
+        .div-17 {
+          margin-top: 40px;
+        }
+      }
+      .div-18 {
+        font-family: Lato, sans-serif;
+        flex-grow: 1;
+        flex-basis: auto;
+      }
+      .img-6 {
+        aspect-ratio: 1;
+        object-fit: auto;
+        object-position: center;
+        width: 23px;
+        align-self: start;
+      }
+      .img-7 {
+        object-fit: auto;
+        object-position: center;
+        width: 396px;
+        margin-top: 16px;
+        max-width: 100%;
+        border: 1px solid rgba(75, 75, 75, 1);
+      }
+      .div-19 {
+        display: flex;
+        margin-top: 67px;
+        width: 375px;
+        max-width: 100%;
+        gap: 20px;
+        font-size: 24px;
+      }
+      @media (max-width: 991px) {
+        .div-19 {
+          margin-top: 40px;
+        }
+      }
+      .div-20 {
+        font-family: Lato, sans-serif;
+        flex-grow: 1;
+        flex-basis: auto;
+      }
+      .img-8 {
+        aspect-ratio: 0.9;
+        object-fit: auto;
+        object-position: center;
+        width: 19px;
+        align-self: start;
+        border: 2px solid rgba(67, 67, 67, 1);
+      }
+      .img-9 {
+        object-fit: auto;
+        object-position: center;
+        width: 396px;
+        margin-top: 18px;
+        max-width: 100%;
+        border: 1px solid rgba(75, 75, 75, 1);
+      }
+      .div-21 {
+        border-radius: 18px;
+        background-color: rgba(56, 145, 201, 1);
+        width: 396px;
+        max-width: 100%;
+        color: rgba(255, 255, 255, 1);
+        white-space: nowrap;
+        text-align: center;
+        margin: 53px 0 13px;
+        padding: 19px 60px;
+        font: 24px Poppins, sans-serif;
+      }
+      @media (max-width: 991px) {
+        .div-21 {
+          margin-top: 40px;
+          white-space: initial;
+          padding: 0 20px;
+        }
+      }
+    `}</style>
+  </>
+);
+}
